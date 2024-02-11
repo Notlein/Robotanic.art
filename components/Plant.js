25d300114752fd30d7a899d3c9b85553c43983f3
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-11 17:47:34 -0500

    fds

--- components/Plant.js
+++ components/Plant.js
@@ -6,7 +6,7 @@ export default function Plantobject() {
   return (
     <>
     <div className={styles.ccanvas_container_div} id="canvas_container_div">
-    <canvas className={styles.ccanvas} id="canvas"></canvas> 
+    <canvas className={styles.ccanvas} id="canvas" width="1000" height="1000"></canvas> 
     </div>
     
 
