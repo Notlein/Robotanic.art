90d51f9739b78da8ac5a553bda4032a61532d9a5
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-10 21:02:01 -0500

    f

--- pages/index.js
+++ pages/index.js
@@ -15,12 +15,12 @@ export default function Home() {
         <p className="description">
           The cyberplant welcomes you. Here are the latest iterations to the <code>struct plant (coming soon...)</code>
         </p>
-        {/* <div style="display:flex">
-          <img src="/img/20231212_175730.jpg" width="auto" height="300px"></img>
-          <img src="/img/20231212_175730.jpg" width="auto" height="300px"></img>
-          <img src="/img/20231212_175730.jpg" width="auto" height="300px"></img>
-          <img src="/img/20231212_175730.jpg" width="auto" height="300px"></img>
-        </div> */}
+        <div style="display:flex">
+          <img src="/img/20231212_175730.jpg"></img>
+          <img src="/img/20231216_132417.jpg"></img>
+          <img src="/img/20231220_162928.jpg"></img>
+          <img src="/img/20240109_172754.jpg"></img>
+        </div> 
       </main>
 
       {/* <Footer /> */}
