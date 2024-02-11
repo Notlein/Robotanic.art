1a24582a03a8a6619f71fc2fc94b031e62b509ad
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-10 20:25:09 -0500

    cyberplant initiated

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Cyber_AGRI_culture</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The cyberplant welcomes you. <code>struct plant {} coming soon... </code>
         </p>
       </main>
 
