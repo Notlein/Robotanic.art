15dad99ec5fc8d41b2c82ae19aee1fe26eb3413d
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-11 18:14:34 -0500

    c

--- public/custom/custom.js
+++ public/custom/custom.js
@@ -482,7 +482,7 @@ function Segment( plant, parentSegment, basePoint1, basePoint2 ) {
   this.spLf2 = null;  // leaf 2 Span
   //skins
   this.skins = [];
-  this.skins.push( addSk( [ this.ptE1.id, this.ptE2.id, this.ptB2.id, this.ptB1.id ], "white" ) );
+  this.skins.push( addSk( [ this.ptE1.id, this.ptE2.id, this.ptB2.id, this.ptB1.id ], "transparent" ) );
 }
 
 ///sun ray constructor
@@ -745,7 +745,7 @@ function renderLeaf( plant, leafSpan ) {
   ctx.lineWidth = 1;
   ctx.lineJoin = "round";
   ctx.lineCap = "round";
-  ctx.strokeStyle = "#408040";
+  ctx.strokeStyle = "#204020";
   ctx.fillStyle = "transparent";
   var ah = 0.75;  // arc height
   //leaf top
@@ -767,7 +767,7 @@ function renderLeaf( plant, leafSpan ) {
   //leaf center
   ctx.beginPath();
   ctx.lineWidth = 0.25;
-  ctx.strokeStyle = "#408040";
+  ctx.strokeStyle = "#204020";
   ctx.moveTo(p1x,p1y);
   ctx.lineTo(p2x,p2y);
   ctx.stroke();
@@ -790,7 +790,7 @@ function renderStalks( plant, segment ) {
     ctx.beginPath();
     ctx.fillStyle = s.color;
     ctx.lineWidth = 0.25;
-    ctx.strokeStyle = "#408040";
+    ctx.strokeStyle = "#204020";
     ctx.moveTo(s.points[0].cx, s.points[0].cy);
     for(var j=1; j<s.points.length; j++) { ctx.lineTo(s.points[j].cx, s.points[j].cy); }
     ctx.lineTo(s.points[0].cx, s.points[0].cy);
@@ -837,7 +837,7 @@ function renderShadows() {
     ctx.lineTo( sh.p3.cx, sh.p3.cy );
     ctx.lineTo( sh.p4.cx, sh.p4.cy );
     ctx.lineTo( sh.p1.cx, sh.p1.cy );
-    ctx.fillStyle = "rgba( 0, 64, 0, 0.1 )";
+    ctx.fillStyle = "rgba( 0, 16, 0, 0.1 )";
     ctx.fill();  
   }
   //resets shadows
