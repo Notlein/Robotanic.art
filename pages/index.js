90c1242df1a1152220eb95cf0f2a09b65d9e2b9c
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-11 15:57:01 -0500

    c

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <Head>
         <title>Cyber_AGRI_culture</title>
         <link rel="icon" href="/favicon.ico" />
-        <script src="public/custom/custom.js" />
+        <script src="/custom/custom.js" />
       </Head>
 
       <main>
