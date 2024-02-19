ab705e5a87de326260ed4d97125b022f7493d357
Maxime-Alexandre Gosselin <61528391+Notlein@users.noreply.github.com>
2024-02-19 17:33:59 -0500

    text revised

--- components/Content.js
+++ components/Content.js
@@ -4,7 +4,7 @@ export default function Content() {
   return (
     <>
       <div className={styles.content}>
-      <h3>A historical framework : Felix Guattari's <em>Les Trois Écologies</em></h3>
+      <h3>A historical reference : Felix Guattari's <em>Les Trois Écologies</em></h3>
       <p><span><text>
       In the light of Félix Guattari’s book The Three Ecologies (Guattari, 1987), which proposes a philosophical framework to think of personal, social, and environmental ecologies as a single system, we can think of the virtual world as a fourth ecology, in which cyberculture and Nature can intersect and co-exist. We can then design sustainable solutions using the Internet of Things while being cautious about our own agency on ecosystems. In this sense, is an Internet of Plant (IoP) as an agricultural model  (Steeneken et al., 2023), feasible, viable and sustainable in the long term?</text>
         <text>
@@ -13,13 +13,13 @@ export default function Content() {
         </p>
         <h3>Alice Jarry's work on sustainability of resources and the micro-electronic aesthetics</h3>
       <p><span>
-        Jarry's work on sustainability and glass waste reflect my own concern with similar themes, such as energy efficiency, recycling design, and e-waste. As the inclusion of recycled material in Jarry's work fosters the idea of design for disassembly, we can draw the parallel with the current state of my work, which has been designed from recycled wood obtained from a discarded bed bunk.
+        Jarry's work on sustainability and glass waste reflect my own concern with similar themes, such as energy efficiency, recycling design, and e-waste. As the inclusion of recycled material in Jarry's work fosters the idea of design for disassembly, we can draw the parallel with the current state of my work, which has been designed with the inclusion of recycled wood, obtained from a discarded bed bunk, and some recycled electronic components. Ultimately, my work echoes Jarry's work as they're both an ode to the micro-controller and the wire aesthetic, as all components and their interactions are revealed for the audience to see.
         </span>
         </p>
       <p><img src="/img/alicejarry.png" className={styles.img}></img></p>
       <h3>Michael Hansmeyer's work on L-systems in design and architecture</h3>
       <p><span>
-      Hansmeyer's work on L-systems and computability in architecture inspires this project both in its design and aesthetic, as well as its 3D representation. The construction of L-systems, or Lindenmayer system, is used to reproduce organic qualities and attribute them to computational model. This can then be transferred into architecture, and abstract 3D modeling, but also can be fine-tuned to realistic models.
+      Hansmeyer's work on L-systems and computability in architecture inspires this project both in its design and aesthetic, as well as its 3D representation. The construction of L-systems, or Lindenmayer system, is used to reproduce organic qualities and attribute them to computational model. This can then be transferred into architecture, and abstract 3D modeling, but also can be fine-tuned to produce realistic biological models. Hansmeyer's experimentation with these systems is certainly similar to the final visual result I am aiming for. Ultimately, his writing on L-systems will prove to be a great resource for the development of this project.
 
         </span>
         </p>
